89f2187f59e922a5152f896f60633ec4b439292a
imaginarydevelopment <imaginarydevelopment@googlemail>
2013-08-16 21:27:20 -0400

    redone for auth, since empty project had none.

--- K94Warriors/Global.asax.cs
+++ K94Warriors/Global.asax.cs
@@ -22,6 +22,7 @@ namespace K94Warriors
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
         }
     }
 }
