206b2179daf8f7e12c48d9dc0b764663e29bc298
James Blair <jmblair7@gmail.com>
2013-08-16 23:23:42 -0400

    added files

---
+++ K94Warriors.Models/DogMedicalRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace K94Warriors.Models
+{
+    public class DogMedicalRecord
+    {
+        [Key]
+        public int RecordID { get; set; }
+
+        public int DogProfileID { get; set; }
+        public virtual DogProfile DogProfile { get; set; }
+
+        public string RecordType { get; set; }
+
+        public string Title { get; set; }
+
+        public byte[] RecordBytes { get; set; }
+
+        public string RecordText { get; set; }
+
+        public DateTime RecordExpiration { get; set; }
+
+        public virtual ICollection<MetaData> MetaData { get; set; }
+    }
+}
---
+++ K94Warriors/EFRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+
+namespace K94Warriors
+{
+    public class EFRepository<T> : IRepository<T>
+        where T : class
+    {
+        private readonly DbContext _dbContext;
+        protected DbContext DbContext { get { return _dbContext; } }
+
+        private readonly DbSet<T> _dbSet;
+        protected DbSet<T> DbSet { get { return _dbSet; } }
+
+        public EFRepository(DbContext dbContext)
+        {
+            if (dbContext == null)
+                throw new ArgumentNullException("dbContext");
+
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return DbSet;
+        }
+
+        public T GetById(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            var dbObj = DbContext.Entry(entity);
+            dbObj.State = EntityState.Modified;
+            DbContext.SaveChanges();
+        }
+
+        public void Insert(T entity)
+        {
+            DbSet.Add(entity);
+            DbContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            var dbObj = DbContext.Entry(entity);
+            dbObj.State = EntityState.Deleted;
+            DbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entity = DbSet.Find(id);
+            if (entity == null) return;
+
+            var dbOjb = DbContext.Entry(entity);
+            dbOjb.State = EntityState.Deleted;
+            DbContext.SaveChanges();
+        }
+    }
+}
