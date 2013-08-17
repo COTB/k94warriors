2c8515db7256f362f0b0144c34b106ea12f73723
James Blair <jmblair7@gmail.com>
2013-08-17 00:42:47 -0400

    missing files

--- K94Warriors/Controllers/AdminController.cs
+++ K94Warriors/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using K94Warriors.Models;
 
 namespace K94Warriors.Controllers
 {
@@ -23,7 +24,7 @@ namespace K94Warriors.Controllers
             throw new NotImplementedException();
         }
 
-        public ActionResult CreateOrUpdateWarrior(Warrior warrior)
+        public ActionResult CreateOrUpdateWarrior(WarriorInfo warrior)
         {
             throw new NotImplementedException();
         }
--- K94Warriors/Controllers/DogController.cs
+++ K94Warriors/Controllers/DogController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
-using K94Warriors.Entities;
+using K94Warriors.Models;
 
 namespace K94Warriors.Controllers
 {
