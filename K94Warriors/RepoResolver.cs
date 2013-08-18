e04eafba63b81ed0bc12a12fdefc46f3e476c1a8
John Grant <jgrant@bitbucket.org>
2013-08-17 22:31:48 -0400

    GetDogs and DeleteDogs changes...dog!

--- K94Warriors/RepoResolver.cs
+++ K94Warriors/RepoResolver.cs
@@ -4,7 +4,13 @@
     {
         public static IRepository<T> GetRepository<T>() where T : class
         {
-            var dbContext = new K9DbContext("K9");
+            var dbContext = new K9DbContext("k94warriors_dbEntities");
+            return new EFRepository<T>(dbContext);
+        }
+
+        public static IRepository<T> GetRepository<T>(string connectionName) where T : class
+        {
+            var dbContext = new K9DbContext(connectionName);
             return new EFRepository<T>(dbContext);
         }
     }
