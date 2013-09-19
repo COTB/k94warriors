c21ab9d6243d46186134881d4df5f955bcef784e
cuongvo <f@fwtf.com>
2013-09-19 12:54:24 -0400

    sdffddf

--- K94Warriors/Migrations/201309191651599_ChangedAgeToBirthYear.Designer.cs
+++ K94Warriors/Migrations/201309191651599_ChangedAgeToBirthYear.Designer.cs
@@ -5,13 +5,13 @@ namespace K94Warriors.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    public sealed partial class Changes : IMigrationMetadata
+    public sealed partial class ChangedAgeToBirthYear : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Changes));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(ChangedAgeToBirthYear));
         
         string IMigrationMetadata.Id
         {
-            get { return "201309191648241_Changes"; }
+            get { return "201309191651599_ChangedAgeToBirthYear"; }
         }
         
         string IMigrationMetadata.Source
