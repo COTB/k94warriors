40c354b43c9edaf268ad822a3f7249d4f4df4ec6
Paul Irwin <paulirwin@gmail.com>
2013-09-26 18:15:52 -0400

    Initial incomplete work on medication log

---
+++ K94Warriors/ViewModels/Medication/MedicationPrintLogViewModel.cs
@@ -0,0 +1,15 @@
+﻿using K94Warriors.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace K94Warriors.ViewModels.Medication
+{
+    public class MedicationPrintLogViewModel
+    {
+        public DogProfile DogProfile { get; set; }
+
+        public IList<DogMedication> Medications { get; set; }
+    }
+}
