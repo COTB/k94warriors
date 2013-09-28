34df6d964b7c6c1849c5d39a3d3a09b57795b79e
Paul Irwin <paulirwin@gmail.com>
2013-09-28 11:43:44 -0400

    Add DogProfile model binding, medications page
    many pages still broken

---
+++ K94Warriors/Models/Binders/DogProfileModelBinder.cs
@@ -0,0 +1,29 @@
+﻿using K94Warriors.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace K94Warriors.Models.Binders
+{
+    public class DogProfileModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            string dogProfileIdParam = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).AttemptedValue;
+
+            int dogProfileId;
+
+            if (!int.TryParse(dogProfileIdParam, out dogProfileId))
+                return null;
+
+            // hack, can't inject here AFAIK
+            var repo = DependencyResolver.Current.GetService<IRepository<DogProfile>>();
+
+            var dog = repo.GetById(dogProfileId);
+
+            return dog;
+        }
+    }
+}
