0664204a41e7f7c0883865361211988cf98ee11a
Paul Irwin <paulirwin@gmail.com>
2013-09-29 09:56:23 -0400

    Get dog creation working again

--- K94Warriors/Models/Binders/DogProfileModelBinder.cs
+++ K94Warriors/Models/Binders/DogProfileModelBinder.cs
@@ -7,23 +7,31 @@ using System.Web.Mvc;
 
 namespace K94Warriors.Models.Binders
 {
-    public class DogProfileModelBinder : IModelBinder
+    public class DogProfileModelBinder : DefaultModelBinder
     {
-        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
-            string dogProfileIdParam = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).AttemptedValue;
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+
+            if (value == null)
+                return base.BindModel(controllerContext, bindingContext);
+
+            string dogProfileIdParam = value.AttemptedValue;
+
+            if (string.IsNullOrEmpty(dogProfileIdParam))
+                return base.BindModel(controllerContext, bindingContext);
 
             int dogProfileId;
 
             if (!int.TryParse(dogProfileIdParam, out dogProfileId))
-                return null;
+                return base.BindModel(controllerContext, bindingContext);
 
             // hack, can't inject here AFAIK
             var repo = DependencyResolver.Current.GetService<IRepository<DogProfile>>();
 
             var dog = repo.GetById(dogProfileId);
 
-            return dog;
+            return dog ?? base.BindModel(controllerContext, bindingContext);;
         }
     }
 }
