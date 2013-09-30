f04381307b15cf9e87b343fef8f23ebe96b9035c
James Blair <jmblair7@gmail.com>
2013-09-30 12:44:10 -0400

    added migration for dognoteattachments

---
+++ K94Warriors/Migrations/201309301642095_DogNoteAttachmentsAndDiscriminatorCol.cs
@@ -0,0 +1,35 @@
+namespace K94Warriors.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DogNoteAttachmentsAndDiscriminatorCol : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.DogNoteAttachments",
+                c => new
+                    {
+                        DogNoteAttachmentID = c.Int(nullable: false, identity: true),
+                        DogNoteID = c.Int(nullable: false),
+                        BlobKey = c.Guid(nullable: false),
+                        MimeType = c.String(),
+                        DogNote_NoteID = c.Int(),
+                    })
+                .PrimaryKey(t => t.DogNoteAttachmentID)
+                .ForeignKey("dbo.DogNotes", t => t.DogNote_NoteID)
+                .Index(t => t.DogNote_NoteID);
+            
+            AddColumn("dbo.DogProfiles", "Discriminator", c => c.String(nullable: false, maxLength: 128));
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.DogNoteAttachments", new[] { "DogNote_NoteID" });
+            DropForeignKey("dbo.DogNoteAttachments", "DogNote_NoteID", "dbo.DogNotes");
+            DropColumn("dbo.DogProfiles", "Discriminator");
+            DropTable("dbo.DogNoteAttachments");
+        }
+    }
+}
